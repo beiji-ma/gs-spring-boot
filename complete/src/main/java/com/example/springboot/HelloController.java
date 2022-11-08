dc92ed2cf907b176040df1f3cff27cad5b3d71a0
Beiji Ma <beijim@lap5cg20858dx.se.axis.com>
2022-11-08 08:51:28 +0100

    second commit

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -10,5 +11,8 @@ public class HelloController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
-
+	@GetMapping("/hello/{content}")
+        public String hello(@PathVariable(value = "content") String content) {
+            return "Hello, " + content + "!";
+        }
 }
